62467b6c9570a063420710786cca9be05744a447
karnking <karankotai9183@gmail.com>
2023-11-23 11:32:00 +0530

    added new project

--- src/Components/projects/ProjectsData.js
+++ src/Components/projects/ProjectsData.js
@@ -1,45 +1,10 @@
-export const projectsData = [
-  {
-    project_img: "https://user-images.githubusercontent.com/68837552/251971952-59c5bd5d-08ec-426e-ab2b-d8033c4238be.jpg",
-    project_img_dark: "https://user-images.githubusercontent.com/68837552/273841973-3f14c78b-2154-436f-8b6b-c01fedf848a2.png",
-
-    title: "Alcazar ",
-    description:
-      "This project is built on the idea of Alcazar website, A website based on a figma design, offering a booking platform for hotels and its rooms. Created by Team of 4. The purpose of this project is to showcase our skills and knowledge in HTML, CSS, JavaScript, and Json-Server. Key Features: User Registration, Authentication, Hotel Browsing, Booking, and CRUD Operations.",
-    tech_stack: [
-      {
-        url: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
-        name: "html",
-      },
-      {
-        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
-        name: "css",
-      },
-      {
-        url: "https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg",
-        name: "js",
-      },
-      {
-        url: "https://images.opencollective.com/chakra-ui-pro/61bd1dd/logo/256.png",
-        name: "chakra",
-      },
-      {
-        url: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
-        name: "github",
-      },
-    ],
-    link: "https://alcazartravelling.netlify.app/",
-    github: "https://github.com/karnking/Travel-website",
-  },
-  {
+export const projectsData = [{
     project_img_dark: "https://user-images.githubusercontent.com/68837552/273813508-812c36fa-e519-4fa2-a39c-0ac8be5a9d89.png",
     project_img: "https://user-images.githubusercontent.com/68837552/258499105-22f90776-1bd8-4b22-947e-48796d97152d.png",
 
     title: "Cryptoshere",
-    description:
-      "CryptoSphere is a state-of-the-art cryptocurrency website designed to be your ultimate guide and resource in the exciting world of digital assets. Our platform offers a comprehensive suite of tools and features to simplify your crypto journey and keep you informed about the latest trends and developments. Made possible with React Library and Redux Toolkit along with Material UI.",
-    tech_stack: [
-      {
+    description: "CryptoSphere is a state-of-the-art cryptocurrency website designed to be your ultimate guide and resource in the exciting world of digital assets. Our platform offers a comprehensive suite of tools and features to simplify your crypto journey and keep you informed about the latest trends and developments. Made possible with React Library and Redux Toolkit along with Material UI.",
+    tech_stack: [{
         url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
         name: "react",
       },
@@ -72,10 +37,8 @@ export const projectsData = [
     project_img_dark: "https://user-images.githubusercontent.com/51919527/243387383-4aa1139b-bc1a-4921-b3e1-115f8d4f40a8.png",
 
     title: "Skin Store",
-    description:
-      "This is clone of SkinStore.com, where we have used HTML, CSS and Javascript to acheive this. In this project we have tried to clone the “SkinStore” website. We had build up all our efforts to do our best in this project. As, the Masai School's Mentorship was specifically to build up our skills and we also accordingly implemented all that teachings in our project to look it at its best.",
-    tech_stack: [
-      {
+    description: "This is clone of SkinStore.com, where we have used HTML, CSS and Javascript to acheive this. In this project we have tried to clone the “SkinStore” website. We had build up all our efforts to do our best in this project. As, the Masai School's Mentorship was specifically to build up our skills and we also accordingly implemented all that teachings in our project to look it at its best.",
+    tech_stack: [{
         url: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
         name: "html",
       },
@@ -98,6 +61,36 @@ export const projectsData = [
     ],
     link: "https://skinkastore.netlify.app/",
     github: "https://github.com/karnking/SkinStore",
+  },
+  {
+    project_img: "https://user-images.githubusercontent.com/68837552/251971952-59c5bd5d-08ec-426e-ab2b-d8033c4238be.jpg",
+    project_img_dark: "https://user-images.githubusercontent.com/68837552/273841973-3f14c78b-2154-436f-8b6b-c01fedf848a2.png",
+
+    title: "Alcazar ",
+    description: "This project is built on the idea of Alcazar website, A website based on a figma design, offering a booking platform for hotels and its rooms. Created by Team of 4. The purpose of this project is to showcase our skills and knowledge in HTML, CSS, JavaScript, and Json-Server. Key Features: User Registration, Authentication, Hotel Browsing, Booking, and CRUD Operations.",
+    tech_stack: [{
+        url: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+        name: "html",
+      },
+      {
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
+        name: "css",
+      },
+      {
+        url: "https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg",
+        name: "js",
+      },
+      {
+        url: "https://images.opencollective.com/chakra-ui-pro/61bd1dd/logo/256.png",
+        name: "chakra",
+      },
+      {
+        url: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        name: "github",
+      },
+    ],
+    link: "https://alcazartravelling.netlify.app/",
+    github: "https://github.com/karnking/Travel-website",
   },
   // {
   //   project_img: "https://user-images.githubusercontent.com/68837552/258520908-0b355fa0-d26f-40f8-9998-c1c4d4f441cf.png",
@@ -140,10 +133,8 @@ export const projectsData = [
     project_img_dark: "https://user-images.githubusercontent.com/68837552/269914781-f50b490f-6f4a-4bf1-b807-74a076f26060.png",
 
     title: "Adventour",
-    description:
-      "Adventour is a MERN stack Adventure booking and hotel reservation application. Offering a booking platform for hotel rooms and exploring details about various hotels in various locations. Along with many options for adventures available in area. This was a group project with a team of 4 people. This is a modern and user-friendly travel app built with React, designed to help users plan and organize their travel adventures with ease.",
-    tech_stack: [
-      {
+    description: "Adventour is a MERN stack Adventure booking and hotel reservation application. Offering a booking platform for hotel rooms and exploring details about various hotels in various locations. Along with many options for adventures available in area. This was a group project with a team of 4 people. This is a modern and user-friendly travel app built with React, designed to help users plan and organize their travel adventures with ease.",
+    tech_stack: [{
         url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
         name: "react",
       },
@@ -152,7 +143,7 @@ export const projectsData = [
         name: "tailwind",
       },
       {
-        url:"https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/20/express-js.png",
+        url: "https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/20/express-js.png",
         name: "express"
       },
       {
@@ -180,10 +171,8 @@ export const projectsData = [
     project_img_dark: "https://user-images.githubusercontent.com/68837552/276520315-5178d85b-1642-42cd-bceb-e6d0665e4ee4.png",
 
     title: "Discord-Clone",
-    description:
-      "About Full-stack project offering real-time communication platform with features like instant messaging, attachment sharing, message editing, 1:1 calls, member management, unique invite links, and a sleek UI. It uses Socket.io for real-time communication.",
-    tech_stack: [
-      {
+    description: "About Full-stack project offering real-time communication platform with features like instant messaging, attachment sharing, message editing, 1:1 calls, member management, unique invite links, and a sleek UI. It uses Socket.io for real-time communication.",
+    tech_stack: [{
         url: "https://www.rlogical.com/wp-content/uploads/2023/03/Rlogical-Blog-Images-thumbnail.webp",
         name: "Nextjs"
       },
@@ -210,5 +199,39 @@ export const projectsData = [
     ],
     link: "https://discord-lite.onrender.com/invite/93a9534d-1cb0-4471-bb3e-4954e5c3ca59",
     github: "https://github.com/karnking/discord-clone",
+  },
+  {
+    project_img: "https://user-images.githubusercontent.com/68837552/279716150-42814da8-dbd2-44b2-935c-ad20c17922e9.png",
+    project_img_dark: "https://user-images.githubusercontent.com/68837552/285110254-bd607a38-5d7b-4c95-8459-9e54c80dc650.png",
+
+    title: "Masai Flow",
+    description: "Revamping Masai School's site with React, we blend original aesthetics with enhanced functionality. Users access course info and live MSAT, with events section showing details and upcoming events. Our rewarding system lets users earn redeemable points for invites or MSAT success.",
+    tech_stack: [{
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+        name: "react",
+      },
+      {
+        url: "https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/20/express-js.png",
+        name: "express"
+      },
+      {
+        url: "https://pbs.twimg.com/profile_images/1452637606559326217/GFz_P-5e_400x400.png",
+        name: "MongoDB"
+      },
+      {
+        url: "https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg",
+        name: "js",
+      },
+      {
+        url: "https://images.opencollective.com/chakra-ui-pro/61bd1dd/logo/256.png",
+        name: "chakra",
+      },
+      {
+        url: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        name: "github",
+      },
+    ],
+    link: "https://masai-flow.vercel.app/",
+    github: "https://github.com/karnking/Masai-Flow",
   },
 ];
